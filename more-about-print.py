faf7ccf6a54c13b96b92ec8f4abbd4f006d810a1
quota311 <hksingh1044@gmail.com>
2024-10-20 15:01:29 -0400

    two commits

---
+++ more-about-print.py
@@ -0,0 +1,10 @@
+#you cannot have two print statements in python. see next print statement
+#print('I\'m studying Python')print('and having fun')
+
+#when you separate multiple strings with a comma, python automatically adds a space
+
+print('I\'m studying Python', 'and having fun')
+
+#use \n to enter a new line
+
+print('I\'m studying\nPython')
