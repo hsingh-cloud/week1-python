845be257728ad75946e40135007d18517ca9a738
quota311 <hksingh1044@gmail.com>
2024-10-20 17:39:11 -0400

    python course tuesday

---
+++ tuesday-variables.py
@@ -0,0 +1,32 @@
+print('I\'m learning Python.\nI\'m practicing coding in Python.')
+
+#module 1 - variables
+#variables are bit like excel cells. similar concept in python.
+#in python, each variable has a name and a value.
+
+#below we define a variable named 'greeting'
+greeting = 'Hello, friend!'
+
+#now, we can perform functions using that variable
+print(greeting)
+
+#we did not put apostrophes around the word 'greeting' because it would print the word 'greeting'
+print('greeting')
+
+greeting = 'Hi, everybody'
+print(greeting)
+
+#variable exists when you first assign a value to it. 
+#must assign a value to a variable before using it in a function.
+new_greeting = 'Cheers!'
+print(new_greeting)
+
+#rules for defining variables are:
+#variable names must start with a letter or the underscore sign
+#upper and lower letters are treated differently
+#a couple of keywords cannot be used as variable names such as: from, global
+
+test_variable_name = 'varible naming rules'
+print(test_variable_name)
+
+print('I\'m learning Python.\nI\'m practicisng my coding skills.')
